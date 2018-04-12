205504f53d7073add0c95220a86c5633a1c332fc
Steph Schouman <svschou@umich.edu>
2018-04-11 22:06:57 -0400

    Made function

--- practice_api.py
+++ practice_api.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
+def get_hp_data(char_name):
+	base_url = "https://www.potterapi.com/v1/"
+	hp_api_key = "$2a$10$XPnZrHnIYgf.R9etCbM/8eHqwCnygF9MlSVbcVA4wDlPsIZpwsZa2"
 
-base_url = "https://www.potterapi.com/v1/"
-hp_api_key = "$2a$10$XPnZrHnIYgf.R9etCbM/8eHqwCnygF9MlSVbcVA4wDlPsIZpwsZa2"
+	params = {"key":hp_api_key,"name": char_name}
 
-params = {"key":hp_api_key,"name": "Harry Potter"}
+	response = requests.get(base_url+"characters",params=params)
+	hp_list = json.loads(response.text) 
 
-response = requests.get(base_url+"characters",params=params)
-hp_list = json.loads(response.text) 
+	return response.text
 
-print(response.text)
+print(get_hp_data("Harry Potter"))
+print(get_hp_data("Hermione Granger"))
